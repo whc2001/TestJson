d6c3653940df9982fc3eaab47d37916fc0d3dc36
whc2001 <whcljh233@gmail.com>
2016-05-27 13:10:04 +0800

    Fix 20160527
    Add comments.
    Write to file instead of print to console.

--- TestJs/Program.cs
+++ TestJs/Program.cs
@@ -9,10 +9,10 @@ namespace TestJs
 {
 	class Program
 	{
-		private static Dictionary<string,SwordModel> dic = new Dictionary<string, SwordModel>();
-		private static Dictionary<string,SwordModel> orderedDic = new Dictionary<string, SwordModel>();
+		private static Dictionary<string,SwordModel> dic = new Dictionary<string, SwordModel>();			//Sword list.
+		private static Dictionary<string,SwordModel> orderedDic = new Dictionary<string, SwordModel>();		//Sorted sword list.
 		
-		public class SwordModel
+		private class SwordModel																			//Json model(used to deserialize json).								
 		{
 			public string name;
 			public int id;
@@ -24,7 +24,7 @@ namespace TestJs
 			public int upgrade;
 		}
 		
-		private static string FormatJson(string str)
+		private static string FormatJson(string str)														//Indent a json.
 		{
 			JsonSerializer serializer = new JsonSerializer();
 			TextReader tr = new StringReader(str);
@@ -35,6 +35,7 @@ namespace TestJs
 				StringWriter textWriter = new StringWriter();
 				JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
 				{
+					
 					Formatting = Formatting.Indented,
 					Indentation = 4,
 					IndentChar = ' '
@@ -48,7 +49,7 @@ namespace TestJs
 			}
 		}
 		
-		private static int FindLast(string key,string source,int startIndex = 0)
+		private static int Find(string key,string source,int startIndex = 0)								//Find string and return the index of last char.
 		{
 			int index = source.IndexOf(key,startIndex);
 			return index + key.Length;
@@ -58,25 +59,31 @@ namespace TestJs
 		{
 			StreamReader sr = new StreamReader(Environment.CurrentDirectory + @"\define.js");
 			string textData = sr.ReadToEnd();
-			int head = FindLast("exports.tohken.define.tohkens = ",textData);
-			int tail = FindLast("};",textData,head) - 1;
-			string data2 = textData.Substring(head,tail - head).Trim();
-			//JObject jo = JObject.Parse(data2);
-			dic = JsonConvert.DeserializeObject<Dictionary<string,SwordModel>>(data2);
+			sr.Close();
+			sr.Dispose();
 			
+			int head = Find("exports.tohken.define.tohkens = ",textData);									//Start index.
+			int tail = Find("};",textData,head) - 1;														//End index.
+			string data2 = textData.Substring(head,tail - head).Trim();										//Js between the two indexs.These text can be parsed as a json.
+			dic = JsonConvert.DeserializeObject<Dictionary<string,SwordModel>>(data2);						//Parse the text and deserialize each item into the list.
 			dic.Add("0",new SwordModel(){ name = "SANIWA",id = 0,rarity = 5,type = 900,group = 900,equip = 3,area = 1,upgrade = 25});
+																											//Not a valid sword.Just for test.
 			
-			foreach(KeyValuePair<string,SwordModel> item in dic.OrderBy(e => int.Parse(e.Key)))
+			foreach(KeyValuePair<string,SwordModel> item in dic.OrderBy(e => int.Parse(e.Key)))				//Sort.
 			{
 				orderedDic.Add(item.Key,item.Value);
 			}
 			
 			string newJson = FormatJson(JsonConvert.SerializeObject(orderedDic));
-			textData = textData.Remove(head,tail - head);
-			textData = textData.Insert(head,newJson);
+			textData = textData.Remove(head,tail - head);													//Remove old js.
+			textData = textData.Insert(head,newJson);														//Insert new js.
+			textData = textData.Replace("\r","");															//Just keep LF.
 			
-			Console.WriteLine(textData);
-			Console.ReadKey();
+			StreamWriter sw = new StreamWriter(Environment.CurrentDirectory + @"\_define.js");				//Write file(for test,write to another file).
+			sw.BaseStream.SetLength(0);
+			sw.Write(textData);
+			sw.Close();
+			sw.Dispose();
 		}
 	}
 }
